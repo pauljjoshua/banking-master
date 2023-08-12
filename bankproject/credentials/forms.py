d6de77414350f3ed4a12ffbf44d0cf915bfff3e7
paul <pauljjoshua@gmail.com>
2023-08-12 13:35:47 +0530

    Initial commit

---
+++ bankproject/credentials/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.forms import ModelForm
+
+from .models import details
+
+# class detailsForm(forms.Form):
+#     class Meta:
+#         model = details
+#         fields = ['name', 'date_of_birth', 'gender', 'mob_no', 'age', 'email', 'address']
+
+
